c8bcc1c0c9a86d4da777bafa9b15e1c432bb6ed0
Sean Fong <seancfong@gmail.com>
2025-02-13 00:24:25 -0800

    style: added basic stylesheet variables and mixins

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -1,5 +1,6 @@
 import Landing2025 from "@components/seasons/2025/landing/landing-2025";
 
 export default function LandingPage() {
+  // To archive the 2025 pages, create an /archive/2025 folder and move the pages there to keep them for future reference.
   return <Landing2025 />;
 }
