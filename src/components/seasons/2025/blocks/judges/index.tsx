d46b0c37fcd10724d64ca7fbcaecf92ac2c550d9
Sean Fong <seancfong@gmail.com>
2025-04-14 16:53:10 -0700

    feat: add judges and speakers

--- src/components/seasons/2025/blocks/judges/index.tsx
+++ src/components/seasons/2025/blocks/judges/index.tsx
@@ -1,27 +1,52 @@
-import JudgeCard from "@components/seasons/2025/blocks/judges/judge-card";
+import ProfileCard from "@components/seasons/2025/blocks/judges/profile-card";
+import { useInView } from "motion/react";
+import { useRef } from "react";
 
 export interface Judge {
   name: string;
   position: string;
-  websiteURL: string;
   linkedInURL: string;
   imageURL: string;
 }
 
 function Judges() {
   return (
-    <section className="noise flex justify-center bg-(--blue) pb-12 lg:pb-20">
-      <div className="container text-white">
-        <h2 className="font-title text-3xl leading-loose font-bold sm:text-4xl lg:text-5xl xl:text-6xl">
-          Judges
-        </h2>
-        <div className="grid grid-cols-1 place-items-center gap-4 sm:grid-cols-2 lg:grid-cols-4">
-          {judges.map((judge, index) => (
-            <JudgeCard key={index} judge={judge} />
-          ))}
-        </div>
-      </div>
+    <section className="noise flex flex-col items-center gap-4 bg-(--blue) pb-12 sm:gap-20 lg:pb-20">
+      <JudgeSection title="Judges" profiles={judges} />
+      <JudgeSection title="Keynote Speakers" profiles={speakers} />
     </section>
+  );
+}
+
+type JudgeSectionProps = {
+  title: string;
+  profiles: Judge[];
+};
+
+function JudgeSection({ title, profiles }: JudgeSectionProps) {
+  const ref = useRef<HTMLDivElement>(null);
+
+  const isInView = useInView(ref, {
+    once: true,
+    margin: "0px 0px -300px 0px",
+  });
+
+  return (
+    <div className="container text-white" ref={ref}>
+      <h2 className="pb-8 text-center font-title text-3xl leading-loose font-bold sm:text-left sm:text-4xl lg:text-5xl xl:text-6xl">
+        {title}
+      </h2>
+      <div className="flex flex-wrap items-stretch justify-center gap-x-8 gap-y-8">
+        {profiles.map((profile, index) => (
+          <ProfileCard
+            key={index}
+            profile={profile}
+            isInView={isInView}
+            index={index}
+          />
+        ))}
+      </div>
+    </div>
   );
 }
 
@@ -29,35 +54,82 @@ export default Judges;
 
 const judges: Judge[] = [
   {
-    name: "Charithra Sathyanarayan",
-    position: "UX Design Lead @ SAP",
-    websiteURL: "#undefined",
-    linkedInURL: "#undefined",
+    name: "Nicole Liang",
+    position: "Senior UX Designer at Google",
+    linkedInURL: "https://www.linkedin.com/in/nicoleliangmai",
     imageURL:
-      "https://res.cloudinary.com/ucidesignathon/image/upload/f_auto,q_auto,w_512,h_512/v1742792000/2025/landing-page/assets/judges/Charithra_Sathyanarayan.png",
+      "https://res.cloudinary.com/ucidesignathon/image/upload/f_auto,q_auto,w_512,h_512/v1744668341/2025/landing-page/assets/judges/IMG_1859_-_Mai_Liang.jpg",
   },
   {
-    name: "Jacky Dittkowski",
-    position: "UX Design Lead @ SAP",
-    websiteURL: "#undefined",
-    linkedInURL: "#undefined",
+    name: "Michael Auld",
+    position: "Product Illustrator at Anduril",
+    linkedInURL: "https://www.linkedin.com/in/michaellauld/",
     imageURL:
-      "https://res.cloudinary.com/ucidesignathon/image/upload/f_auto,q_auto,w_512,h_512/v1742792000/2025/landing-page/assets/judges/Jacky_Dittkowski.png",
+      "https://res.cloudinary.com/ucidesignathon/image/upload/f_auto,q_auto,w_512,h_512/v1744668808/2025/landing-page/assets/judges/bwbackground_-_Michael_Auld.jpg",
   },
   {
-    name: "Shana Sanford",
-    position: "Product Designer @ Uber",
-    websiteURL: "#undefined",
-    linkedInURL: "#undefined",
+    name: "Sergio Harashyn",
+    position: "Co-founder, Head of Product Design at AI2UX",
+    linkedInURL: "https://www.linkedin.com/in/harashyn/",
     imageURL:
-      "https://res.cloudinary.com/ucidesignathon/image/upload/f_auto,q_auto,w_512,h_512/v1742792000/2025/landing-page/assets/judges/Shana_Sanford.png",
+      "https://res.cloudinary.com/ucidesignathon/image/upload/f_auto,q_auto,w_512,h_512/v1744669080/2025/landing-page/assets/judges/sergio-harashyn.jpg",
   },
   {
-    name: "Nikhil Mehrotra",
-    position: "UX Designer @ Indeed",
-    websiteURL: "#undefined",
-    linkedInURL: "#undefined",
+    name: "Sarina Chen",
+    position: "Growth Product Designer & Manager at MoBagel",
+    linkedInURL: "https://www.linkedin.com/in/yiwen-sarina-chen/",
     imageURL:
-      "https://res.cloudinary.com/ucidesignathon/image/upload/f_auto,q_auto,w_512,h_512/v1742792000/2025/landing-page/assets/judges/Nikhil_Mehrotra.png",
+      "https://res.cloudinary.com/ucidesignathon/image/upload/f_auto,q_auto,w_512,h_512/v1744669962/2025/landing-page/assets/judges/sarina-chen.png",
+  },
+  {
+    name: "Agnes Tran",
+    position: "Senior Designer at IBM iX",
+    linkedInURL: "https://www.linkedin.com/in/agnestran/",
+    imageURL:
+      "https://res.cloudinary.com/ucidesignathon/image/upload/f_auto,q_auto,w_512,h_512/v1744670942/2025/landing-page/assets/judges/agnes-tran.png",
+  },
+  {
+    name: "Kyuha Jung",
+    position: "PhD Student & HCI Researcher at UCI Informatics",
+    linkedInURL: "https://www.linkedin.com/in/jkyuha/",
+    imageURL:
+      "https://res.cloudinary.com/ucidesignathon/image/upload/f_auto,q_auto,w_512,h_512/v1744671032/2025/landing-page/assets/judges/KyuhaJung_20241130_-_Kyuha_Jung.jpg",
+  },
+  {
+    name: "Yuko Shimura",
+    position: "Product Designer at Centerfield",
+    linkedInURL: "https://www.linkedin.com/in/yuko-shimura/",
+    imageURL:
+      "https://res.cloudinary.com/ucidesignathon/image/upload/f_auto,q_auto,w_512,h_512/v1744671249/2025/landing-page/assets/judges/yuko-shimura.png",
+  },
+  {
+    name: "Iris Spear",
+    position: "Lead UX Designer at Alteryx",
+    linkedInURL: "https://www.linkedin.com/in/iris-spear/",
+    imageURL:
+      "https://res.cloudinary.com/ucidesignathon/image/upload/f_auto,q_auto,w_512,h_512/v1744671472/2025/landing-page/assets/judges/iris-spear.png",
+  },
+];
+
+const speakers: Judge[] = [
+  {
+    name: "Robyn Young",
+    position: "",
+    linkedInURL: "",
+    imageURL: "",
+  },
+  {
+    name: "Alex Park",
+    position: "UX Design Manager at Riot Games",
+    linkedInURL: "https://www.linkedin.com/in/alexpark5/",
+    imageURL:
+      "https://res.cloudinary.com/ucidesignathon/image/upload/f_auto,q_auto,w_512,h_512/v1744674718/2025/landing-page/assets/judges/alex-me_-_Alex_Park.jpg",
+  },
+  {
+    name: "Alex Park",
+    position: "UX Design Manager at Riot Games",
+    linkedInURL: "https://www.linkedin.com/in/alexpark5/",
+    imageURL:
+      "https://res.cloudinary.com/ucidesignathon/image/upload/f_auto,q_auto,w_512,h_512/v1744674718/2025/landing-page/assets/judges/alex-me_-_Alex_Park.jpg",
   },
 ];
---
+++ src/components/seasons/2025/blocks/judges/profile-card.tsx
@@ -0,0 +1,94 @@
+import { cn } from "@/lib/utils";
+import DOTImage from "@components/common/dot-image";
+import { Judge } from "@components/seasons/2025/blocks/judges";
+import { Card, CardContent, CardHeader } from "@components/ui/card";
+import Link from "next/link";
+
+type Props = {
+  profile: Judge;
+  isInView: boolean;
+  index: number;
+};
+
+function ProfileCard({ profile, isInView, index }: Props) {
+  return (
+    <Card
+      className={cn(
+        "max-w-[350px] basis-[100%] translate-y-10 rounded-full border-0 bg-white/60 p-8 opacity-0 transition-all duration-500 ease-out-quart",
+        { "translate-y-0 opacity-100": isInView },
+      )}
+      style={{
+        transitionDelay: `${index * 50}ms`,
+      }}
+    >
+      <CardHeader className="relative aspect-square overflow-hidden rounded-full bg-[#FEFCEB] p-0">
+        <DOTImage
+          alt={profile.name}
+          src={profile.imageURL}
+          fill
+          sizes="(min-width: 0px) 50vw; (min-width: 768px) 25vw"
+        />
+      </CardHeader>
+      <CardContent className="flex flex-grow flex-col items-center justify-between gap-6 pb-4 text-center text-[#824A32]">
+        <div className="space-y-2">
+          <h3
+            className="text-center text-2xl leading-[1.1] font-bold lg:text-3xl"
+            // Force newline between first and last name
+            style={{ wordSpacing: "100vw" }}
+          >
+            {profile.name}
+          </h3>
+          <p className="lg:text-base3xl:text-xl text-sm font-medium 3xl:text-lg">
+            {profile.position}
+          </p>
+        </div>
+        <div className="flex items-center gap-2">
+          <Link
+            href={profile.linkedInURL}
+            className="rounded-full bg-[#654A3F] p-2 text-white"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <LinkedinIcon />
+          </Link>
+        </div>
+      </CardContent>
+    </Card>
+  );
+}
+
+export default ProfileCard;
+
+function LinkedinIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="27"
+      height="27"
+      viewBox="0 0 27 27"
+      fill="none"
+    >
+      <path
+        d="M17.3799 9.23047C19.0985 9.23047 20.7467 9.91318 21.962 11.1284C23.1772 12.3437 23.8599 13.9919 23.8599 15.7105V23.2705H19.5399V15.7105C19.5399 15.1376 19.3123 14.5882 18.9073 14.1831C18.5022 13.778 17.9528 13.5505 17.3799 13.5505C16.807 13.5505 16.2576 13.778 15.8526 14.1831C15.4475 14.5882 15.2199 15.1376 15.2199 15.7105V23.2705H10.8999V15.7105C10.8999 13.9919 11.5826 12.3437 12.7979 11.1284C14.0131 9.91318 15.6613 9.23047 17.3799 9.23047Z"
+        stroke="white"
+        strokeWidth="1.62"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M6.58001 10.3105H2.26001V23.2705H6.58001V10.3105Z"
+        stroke="white"
+        strokeWidth="1.62"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M4.42001 7.07098C5.61295 7.07098 6.58001 6.10391 6.58001 4.91098C6.58001 3.71804 5.61295 2.75098 4.42001 2.75098C3.22707 2.75098 2.26001 3.71804 2.26001 4.91098C2.26001 6.10391 3.22707 7.07098 4.42001 7.07098Z"
+        stroke="white"
+        strokeWidth="1.62"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
