dbb639860b1951c6ffac284e821d525ed84ac0ba
Sean Fong <seancfong@gmail.com>
2025-04-16 20:24:46 -0700

    feat: add itinerary

--- src/components/seasons/2025/blocks/itinerary/index.tsx
+++ src/components/seasons/2025/blocks/itinerary/index.tsx
@@ -1,5 +1,134 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { cn } from "@/lib/utils";
+import {
+  april18,
+  april19,
+  april20,
+  ItineraryItem,
+} from "@components/seasons/2025/blocks/itinerary/itinerary-dates";
+import { ArrowRight } from "lucide-react";
+import { ComponentProps, Fragment } from "react";
+
 function Itinerary() {
-  return <section>Itinerary</section>;
+  return (
+    <section
+      id="past-events"
+      className="noise flex justify-center overflow-x-hidden bg-(--blue) py-16"
+    >
+      <div className="relative container flex w-full flex-col gap-8 py-8 text-white">
+        <h2 className="font-title text-3xl font-bold sm:text-4xl lg:text-5xl xl:text-6xl">
+          Itinerary
+        </h2>
+        <Tabs
+          defaultValue="fri"
+          className="mx-auto max-w-[400px] gap-8 md:max-w-none"
+        >
+          <TabsList className="h-12 !divide-x-2 !divide-(--blue) bg-gradient-to-b from-(--tan) to-(--peach) p-0">
+            <ItineraryTabTrigger value="fri">FRI</ItineraryTabTrigger>
+            <ItineraryTabTrigger value="sat">SAT</ItineraryTabTrigger>
+            <ItineraryTabTrigger value="sun">SUN</ItineraryTabTrigger>
+          </TabsList>
+          <TabsContent value="fri">
+            <ItineraryContent
+              items={april18}
+              title="Friday - April 18th, 2025"
+            />
+          </TabsContent>
+          <TabsContent value="sat">
+            <ItineraryContent
+              items={april19}
+              title="Saturday - April 19th, 2025"
+            />
+          </TabsContent>
+          <TabsContent value="sun">
+            <ItineraryContent
+              items={april20}
+              title="Sunday - April 20th, 2025"
+            />
+          </TabsContent>
+        </Tabs>
+      </div>
+    </section>
+  );
 }
 
 export default Itinerary;
+
+function ItineraryTabTrigger({
+  children,
+  ...props
+}: ComponentProps<typeof TabsTrigger>) {
+  return (
+    <TabsTrigger
+      className={cn(
+        "h-full rounded-none rounded-r-none border-0 px-8 text-lg font-semibold !text-(--blue) transition-none first:rounded-l-md last:rounded-r-md data-[state=active]:bg-(--blue) data-[state=active]:!bg-gradient-to-b data-[state=active]:!from-(--sky) data-[state=active]:!to-(--blue) data-[state=active]:!text-white data-[state=active]:shadow-none",
+      )}
+      {...props}
+    >
+      {children}
+    </TabsTrigger>
+  );
+}
+
+type ItineraryContentProps = {
+  items: ItineraryItem[];
+  title: string;
+};
+
+function ItineraryContent({ items, title }: ItineraryContentProps) {
+  return (
+    <div className="flex flex-col gap-4 font-bold">
+      <div>
+        <p className="text-sm font-normal text-(--peach) italic lg:text-base">
+          All times are in PST.
+        </p>
+        <h3>
+          <span className="text-xl font-bold sm:text-2xl lg:text-4xl">
+            {title}
+          </span>
+        </h3>
+      </div>
+      <div className="flex flex-col gap-4">
+        {items.map((item, index) => (
+          <Fragment key={`${title}-${index}`}>
+            {item.type === "time-mark" && (
+              <div className="flex items-center justify-center gap-2 rounded-xl bg-gradient-to-b from-(--pink) to-(--tan) py-4 text-(--blue) sm:py-6 sm:text-2xl">
+                <span>{item.time}</span>
+                <span>|</span>
+                <span>{item.title}</span>
+              </div>
+            )}
+            {item.type === "event" && (
+              <div
+                className={cn(
+                  "flex flex-col items-stretch justify-center gap-4",
+                  "md:grid md:grid-cols-2 md:gap-8",
+                )}
+              >
+                <div className="flex flex-col justify-center gap-1 rounded-xl bg-gradient-to-b from-[#A6CDF6] to-[#FFFCED] p-4 text-(--blue) sm:p-8">
+                  <span className="text-xl leading-[1.1] font-bold sm:max-w-[350px] sm:text-3xl md:mb-2 lg:max-w-none lg:pr-4 lg:text-4xl">
+                    {item.content.title}
+                  </span>
+                  <span className="mb-2 text-sm font-medium sm:text-base">
+                    {item.content.location}
+                  </span>
+                  <span className="flex items-center gap-1 sm:text-lg">
+                    {item.content.timeStart}{" "}
+                    <ArrowRight className="size-4" strokeWidth={3} />{" "}
+                    {item.content.timeEnd}
+                  </span>
+                </div>
+                <div className="space-y-2 text-sm font-normal sm:text-base md:flex md:flex-col md:justify-center md:text-lg">
+                  {item.description}
+                </div>
+              </div>
+            )}
+            {index !== items.length - 1 && (
+              <hr className="rounded-full border-2 border-(--sky)/20" />
+            )}
+          </Fragment>
+        ))}
+      </div>
+    </div>
+  );
+}
---
+++ src/components/seasons/2025/blocks/itinerary/itinerary-dates.tsx
@@ -0,0 +1,395 @@
+export type ItineraryItem = TimeMarker | Event;
+
+export type TimeMarker = {
+  type: "time-mark";
+  time: string;
+  title: string;
+  note?: string;
+};
+
+export type Event = {
+  type: "event";
+  content: {
+    title: string;
+    location: string;
+    timeStart: string;
+    timeEnd: string;
+  };
+  description: React.ReactNode;
+};
+
+export const april18: ItineraryItem[] = [
+  {
+    type: "time-mark",
+    time: "3:30 PM",
+    title: "Check-In Starts",
+  },
+  {
+    type: "event",
+    content: {
+      title: "Check-In",
+      location: "DCE Courtyard (In-Person)",
+      timeStart: "3:30 PM",
+      timeEnd: "5:00 PM",
+    },
+    description: (
+      <>
+        <p>Please check in to confirm your attendance at DAUCI Designathon!</p>
+        <p>
+          If you are an <strong>in-person participant</strong>, check in at the
+          DCE entrance. Please bring your government or student ID. You will be
+          given a badge you must wear to each day of the event.
+        </p>
+      </>
+    ),
+  },
+  {
+    type: "event",
+    content: {
+      title: "Opening Ceremony and Keynote Speaker + Panel",
+      location: "DCE, Yosemite ABC (In-Person) + Zoom (Online)",
+      timeStart: "4:30 PM",
+      timeEnd: "5:30 PM",
+    },
+    description: (
+      <>
+        <p>
+          Join us for the Opening Ceremony and official kickoff of the 2025
+          DAUCI Designathon and hear our opening keynote from Robyn Young, the
+          Founder of Young & Co. Following this, we will have a keynote speaker
+          panel made up of Alex Park (UX Design Manager at Riot Games), Tyler
+          Vickers (Senior TPM at Amazon), and Miles Seiden (Founder of Miles
+          Seiden Creative).
+        </p>
+        <Host>
+          Led by the DAUCI Designathon Directors Jasmine Wu and Laila Wafaie.
+        </Host>
+      </>
+    ),
+  },
+  {
+    type: "event",
+    content: {
+      title: "Dinner & Team Formation",
+      location: "DCE Courtyard (In-Person)",
+      timeStart: "5:30 PM",
+      timeEnd: "6:30 PM",
+    },
+    description: (
+      <>
+        <p>
+          Enjoy Panda Express (chow mien and fried rice) for dinner alongside
+          some pocky and chips. Mingle, network, and meet fellow designers over
+          dinner! Start forming teams and brainstorming your project ideas.
+        </p>
+      </>
+    ),
+  },
+  {
+    type: "time-mark",
+    time: "6:30 PM",
+    title: "Designing Begins",
+  },
+  {
+    type: "event",
+    content: {
+      title: "Workshop: Basics of Figma & Prototyping with Commit the Change",
+      location: "Yosemite ABC (In-Person) + Zoom (Online)",
+      timeStart: "7:00 PM",
+      timeEnd: "7:30 PM",
+    },
+    description: (
+      <>
+        <p>
+          Learn about how to set up basic frames with a guide on the core tools
+          and best practices when using Figma. We&apos;ll be going over how to
+          organize your Figma workspace, useful plugins, and simple prototyping
+          tips to get any beginner designer a head start on Figma&apos;s
+          software.
+        </p>
+        <Host>Hosted by Commit the Change</Host>
+      </>
+    ),
+  },
+  {
+    type: "time-mark",
+    time: "7:30 PM",
+    title: "Venue Closes",
+  },
+];
+
+export const april19: ItineraryItem[] = [
+  {
+    type: "time-mark",
+    time: "8:00 AM",
+    title: "Check-In Starts",
+  },
+  {
+    type: "event",
+    content: {
+      title: "Check-In & Breakfast",
+      location: "DCE Courtyard (In-Person)",
+      timeStart: "8:00 AM",
+      timeEnd: "9:30 AM",
+    },
+    description: (
+      <>
+        <p>
+          <strong>In person:</strong> Check in and grab a light breakfast of
+          muffins and bagels to kick off your morning before a focused day of
+          designing! Remember to bring your Badges.
+        </p>
+      </>
+    ),
+  },
+  {
+    type: "event",
+    content: {
+      title: "Hiking Social",
+      location: "DCE Courtyard (In-Person)",
+      timeStart: "10:00 AM",
+      timeEnd: "11:00 AM",
+    },
+    description: (
+      <>
+        <p>
+          Start your day with a hike around a beautiful nature scene in Aldrich
+          Park!
+        </p>
+        <Host>Led by Director Jerry Nguyen</Host>
+      </>
+    ),
+  },
+  {
+    type: "event",
+    content: {
+      title: "Workshop: Notion for Building Design Portfolios",
+      location: "DCE, Yosemite ABC (In-Person) + Zoom (Online)",
+      timeStart: "11:00 AM",
+      timeEnd: "12:00 PM",
+    },
+    description: (
+      <>
+        <p>TBA</p>
+        <Host>Hosted by Ella Chung, Notion Campus Ambassador</Host>
+      </>
+    ),
+  },
+  {
+    type: "event",
+    content: {
+      title: "Lunch (with Notion)",
+      location: "DCE Courtyard (In-Person)",
+      timeStart: "12:00 PM",
+      timeEnd: "1:30 PM",
+    },
+    description: (
+      <>
+        <p>
+          Enjoy Mendicino sandwiches and In-N-Out Burgers with Notion and take a
+          well-deserved design break! Mingle with other designers or continue
+          working on your project with the team.
+        </p>
+      </>
+    ),
+  },
+  {
+    type: "event",
+    content: {
+      title:
+        "Workshop: AI by Design: An Approach to Strategic, Human-Centered AI",
+      location: "DCE, Yosemite ABC (In-Person) + Zoom (Online)",
+      timeStart: "1:30 PM",
+      timeEnd: "2:15 PM",
+    },
+    description: (
+      <>
+        <p>
+          As designers and product owners, we have a choice—when, where, how,
+          and at what cost AI shows up in our work and lives. This &quot;AI By
+          Design&quot; session explores foundational AI concepts and enhanced
+          design processes to create AI initiatives that truly add value—and
+          keep you in the conversation.
+        </p>
+        <Host>Led by Erin from FourbyNorth</Host>
+      </>
+    ),
+  },
+  {
+    type: "event",
+    content: {
+      title: "Workshop: [TBA]",
+      location: "DCE, Yosemite ABC (In-Person) + Zoom (Online)",
+      timeStart: "2:15 PM",
+      timeEnd: "2:45 PM",
+    },
+    description: <>TBD</>,
+  },
+  {
+    type: "event",
+    content: {
+      title: "Workshop: [TBA]",
+      location: "DCE, Yosemite ABC (In-Person) + Zoom (Online)",
+      timeStart: "2:45 PM",
+      timeEnd: "3:15 PM",
+    },
+    description: <>TBD</>,
+  },
+  {
+    type: "event",
+    content: {
+      title: "Workshop: [TBA]",
+      location: "DCE, Yosemite ABC (In-Person) + Zoom (Online)",
+      timeStart: "3:15 PM",
+      timeEnd: "3:45 PM",
+    },
+    description: <>TBD</>,
+  },
+  {
+    type: "event",
+    content: {
+      title: "Dinner",
+      location: "DCE Courtyard (In-Person)",
+      timeStart: "6:00 PM",
+      timeEnd: "7:30 PM",
+    },
+    description: (
+      <>
+        <p>
+          Fuel up for the last stretch with a hearty pizza dinner and take a
+          breather with your team.
+        </p>
+      </>
+    ),
+  },
+  {
+    type: "event",
+    content: {
+      title: "Karaoke",
+      location: "DCE, Yosemite ABC (In-Person)",
+      timeStart: "7:30 PM",
+      timeEnd: "8:30 PM",
+    },
+    description: (
+      <>
+        <p>Wind down with karaoke with 2020s bangers!</p>
+        <Host>Hosted by Designathon Director Jerry Nguyen.</Host>
+      </>
+    ),
+  },
+  {
+    type: "time-mark",
+    time: "8:30 PM",
+    title: "Venue Closes",
+  },
+];
+
+export const april20: ItineraryItem[] = [
+  {
+    type: "time-mark",
+    time: "8:00 AM",
+    title: "Check-In Starts",
+  },
+  {
+    type: "event",
+    content: {
+      title: "Check-In and Breakfast",
+      location: "ISEB Patio (In-Person)",
+      timeStart: "8:00 AM",
+      timeEnd: "9:00 AM",
+    },
+    description: (
+      <>
+        <p>
+          <strong>In-person:</strong> Einstein Bagels will be served at the ISEB
+          patio. Please have your badges ready to check off!
+        </p>
+      </>
+    ),
+  },
+  {
+    type: "event",
+    content: {
+      title: "Final Submissions Due on Devpost",
+      location: "Devpost (Online)",
+      timeStart: "9:00 AM",
+      timeEnd: "9:00 AM",
+    },
+    description: (
+      <>
+        <p>
+          All teams must submit their project to Devpost by this time and follow
+          the instructions. This includes submitting the Figma Prototype,
+          indication of online/in-person, writeup, team member names, and
+          optionally a video submission. We will not accept any submissions
+          after this time.
+        </p>
+      </>
+    ),
+  },
+  {
+    type: "event",
+    content: {
+      title: "Judging Fair",
+      location: "ISEB Patio (In-Person) + Devpost (Online)",
+      timeStart: "10:00 AM",
+      timeEnd: "12:00 PM",
+    },
+    description: (
+      <>
+        <p>
+          <strong>In-person:</strong> Present your work science-fair style at
+          assigned tables to our judges from IBM, Anduril, and Google.
+        </p>
+        <p>
+          <strong>Online:</strong> Judges will review your Devpost
+          asynchronously and evaluate based on rubric criteria. Make sure you
+          submit a high quality video beforehand!
+        </p>
+      </>
+    ),
+  },
+  {
+    type: "event",
+    content: {
+      title: "Top 10 Finalist Pitches on Stage",
+      location: "Antrepreneur Center (In-Person) + Zoom (Online)",
+      timeStart: "1:30 PM",
+      timeEnd: "2:30 PM",
+    },
+    description: (
+      <>
+        <p>
+          Each top 10 team will give a 2-3 minute pitch of their product to a
+          live panel of judges. Online teams will present to the judges on zoom
+          at allotted times.
+        </p>
+      </>
+    ),
+  },
+  {
+    type: "event",
+    content: {
+      title: "Closing Ceremony & Winners Announced",
+      location: "Antrepreneur Center (In-Person) + Zoom (Online)",
+      timeStart: "3:00 PM",
+      timeEnd: "3:30 PM",
+    },
+    description: (
+      <>
+        <p>Prizes will be announced for all participants.</p>
+        <Host>Led by Directors Jasmine Wu and Laila Wafaie</Host>
+      </>
+    ),
+  },
+  {
+    type: "time-mark",
+    time: "3:30 PM",
+    title: "Design-a-thon Ends",
+    note: "we will ship prizes to online winners through an online form",
+  },
+];
+
+function Host({ children }: { children: React.ReactNode }) {
+  return <span className="font-medium text-(--peach)">{children}</span>;
+}
