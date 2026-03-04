aee786338ccfc091cb46dbcc71329e77805a2ffd
Jay Chan <jayc10@uci.edu>
2026-03-04 00:56:56 -0800

    feat: about section basis, luxuriousscript added to layout.tsx

--- src/components/seasons/2026/blocks/landing/landing-2026.tsx
+++ src/components/seasons/2026/blocks/landing/landing-2026.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import About from "@components/seasons/2026/blocks/about";
 import FAQ from "@components/seasons/2026/blocks/faq";
 import Footer from "@components/seasons/2026/blocks/footer";
 import Hero from "@components/seasons/2026/blocks/hero";
@@ -12,6 +13,7 @@ export default function Landing2026() {
     <main className="relative min-h-[360vw] w-full bg-[url('/images/seasons/2026/landing/gradient/gradient.png')] bg-[length:100%_auto] bg-top bg-no-repeat">
       <LandingNavigation />
       <Hero />
+      <About />
       <Judges />
       <FAQ />
       <Sponsors />
