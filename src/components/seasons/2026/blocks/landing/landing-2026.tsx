6d5bfc3c2c015dabee8b2636576e62db62f4a985
Allison <allisonlyhuang@gmail.com>
2026-03-05 12:21:00 -0800

    chore: change apply button

--- src/components/seasons/2026/blocks/landing/landing-2026.tsx
+++ src/components/seasons/2026/blocks/landing/landing-2026.tsx
@@ -7,15 +7,14 @@ import Hero from "@components/seasons/2026/blocks/hero";
 import Judges from "@components/seasons/2026/blocks/judges";
 import LandingNavigation from "@components/seasons/2026/blocks/navigation/landing-navigation";
 import Sponsors from "@components/seasons/2026/blocks/sponsors";
-import Link from "next/link"
-
+import Link from "next/link";
 
 export default function Landing2026() {
   return (
     <main className="relative min-h-[360vw] w-full bg-[url('/images/seasons/2026/landing/gradient/background.webp')] bg-[length:100%_auto] bg-top bg-repeat">
       <Link
         href="/2026-preview/apply"
-        className="fixed top-6 right-6 z-50 px-6 py-2 bg-white text-black font-medium rounded hover:bg-gray-200 transition-colors"
+        className="fixed top-6 right-6 z-50 rounded bg-white px-6 py-2 font-medium text-black transition-colors hover:bg-gray-200"
       >
         Apply
       </Link>
