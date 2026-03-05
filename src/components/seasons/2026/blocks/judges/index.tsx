8c83d86c79162d3c9035584552223ef3aa5213a2
Anna Kwan <sufengk@uci.edu>
2026-03-04 22:35:58 -0800

    feat: judges carousel styling and planet images

--- src/components/seasons/2026/blocks/judges/index.tsx
+++ src/components/seasons/2026/blocks/judges/index.tsx
@@ -1,6 +1,8 @@
 "use client";
 import { useInView } from "motion/react";
-import { useRef } from "react";
+import { useCallback, useRef, useState } from "react";
+import DOTImage from "@components/common/dot-image";
+
 
 import {
   Carousel,
@@ -14,86 +16,111 @@ import ProfileCard from "./profile-card";
 export interface Judge {
   name: string;
   position: string;
+  company: string;
   linkedInURL: string;
   imageURL: string;
+}
+
+export interface Planet {
+  id: number;
+  imageURL: string;
+  name: string;
 }
 
 const judges: Judge[] = [
   {
     name: "Nicole Liang",
-    position: "Senior UX Designer at Google",
+    position: "Senior UX Designer",
+    company: "Google",
     linkedInURL: "https://www.linkedin.com/in/nicoleliangmai",
     imageURL: "/images/seasons/2025/landing/judges/nicoleliangmai.jpg",
   },
   {
     name: "Michael Auld",
-    position: "Product Illustrator at Anduril",
+    position: "Product Illustrator",
+    company: "Anduril",
     linkedInURL: "https://www.linkedin.com/in/michaellauld/",
     imageURL: "/images/seasons/2025/landing/judges/michaellauld.jpg",
   },
   {
     name: "Sergio Harashyn",
-    position: "Co-founder, Head of Product Design at AI2UX",
+    position: "Co-founder, Head of Product Design",
+    company: "AI2UX",
     linkedInURL: "https://www.linkedin.com/in/harashyn/",
     imageURL: "/images/seasons/2025/landing/judges/sergio-harashyn.jpg",
   },
   {
     name: "Sarina Chen",
-    position: "Growth Product Designer & Manager at MoBagel",
+    position: "Growth Product Designer & Manager",
+    company: "MoBagel",
     linkedInURL: "https://www.linkedin.com/in/yiwen-sarina-chen/",
     imageURL: "/images/seasons/2025/landing/judges/sarina-chen.png",
   },
   {
     name: "Agnes Tran",
-    position: "Senior Designer at IBM iX",
+    position: "Senior Designer", 
+    company: "IBM iX",
     linkedInURL: "https://www.linkedin.com/in/agnestran/",
     imageURL: "/images/seasons/2025/landing/judges/agnes-tran.png",
   },
   {
     name: "Kyuha Jung",
-    position: "PhD Student & HCI Researcher at UCI Informatics",
+    position: "PhD Student & HCI Researcher", 
+    company: "UCI Informatics",
     linkedInURL: "https://www.linkedin.com/in/jkyuha/",
     imageURL: "/images/seasons/2025/landing/judges/jkyuha.jpg",
   },
   {
     name: "Yuko Shimura",
-    position: "Product Designer at Centerfield",
+    position: "Product Designer",
+    company: "Centerfield",
     linkedInURL: "https://www.linkedin.com/in/yuko-shimura/",
     imageURL: "/images/seasons/2025/landing/judges/yuko-shimura.png",
   },
   {
     name: "Iris Spear",
-    position: "Lead UX Designer at Alteryx",
+    position: "Lead UX Designer",
+    company: "Alteryx",
     linkedInURL: "https://www.linkedin.com/in/iris-spear/",
     imageURL: "/images/seasons/2025/landing/judges/iris-spear.png",
   },
   {
     name: "Miles Seiden",
     position:
-      "Fractional Creative Director & Consultant at Miles Seiden Creative",
+      "Fractional Creative Director & Consultant",
+    company: "Miles Seiden Creative",
     linkedInURL: "https://www.linkedin.com/in/milesseiden/",
     imageURL: "/images/seasons/2025/landing/judges/milesseiden.png",
   },
   {
     name: "Damjan Krajacic",
-    position: "UX Strategist at ActiveColor",
+    position: "UX Strategist",
+    company: "ActiveColor",
     linkedInURL: "https://www.linkedin.com/in/damjankrajacic/",
     imageURL: "/images/seasons/2025/landing/judges/damjankrajacic.jpeg",
   },
   {
     name: "Lawrence Ntim",
-    position: "Ex-Senior Product Designer at Dropbox",
+    position: "Ex-Senior Product Designer",
+    company: "Dropbox",
     linkedInURL: "https://www.linkedin.com/in/lawrencentim/",
     imageURL: "/images/seasons/2025/landing/judges/lawrence.png",
   },
 ];
 
+
 export default function IndexPage() {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const carouselApiRef = useRef<any>(null);
   const sectionRef = useRef<HTMLDivElement | null>(null);
   const isInView = useInView(sectionRef, {
     once: true,
     margin: "0px 0px -300px 0px",
   });
+  const updateActiveIndex = useCallback( () => {
+    if (!carouselApiRef.current) return;
+    setActiveIndex(carouselApiRef.current.selectedScrollSnap())
+  }, [])
 
   return (
     <main className="flex min-h-screen items-center justify-center bg-(--blue) text-white">
@@ -104,26 +131,47 @@ export default function IndexPage() {
         <h2 className="relative z-10 container font-title text-3xl font-bold sm:text-4xl lg:text-5xl xl:text-6xl">
           Judges
         </h2>
-        <Carousel opts={{ loop: true }} className="-mt-8 w-full">
+        <Carousel opts={{ 
+          loop: true,
+          slidesToScroll: 1
+          }} 
+          setApi = { (api) => {
+            carouselApiRef.current = api;
+            api?.on('select', updateActiveIndex);
+            updateActiveIndex();
+          }}
+          className="-mt-8 w-full">
+          <div className="relative">
+            <div className="flex basis justify-center rounded-full opacity-40 pointer-events-none">
+              <DOTImage
+              alt="background glow"
+              src="/images/seasons/2026/landing/judges/sponsor_planet_bg.png"
+              fill
+              className="object-contain scale-[130%]"/>
+              </div>
           <CarouselContent>
             {judges.map((judge, index) => (
               <CarouselItem
                 key={judge.name}
-                className="flex basis-1/2 justify-center py-4"
+                className="flex basis-1/3 justify-center py-4"
               >
                 <div className="origin-center scale-75">
                   <ProfileCard
                     profile={judge}
                     isInView={isInView}
                     index={index}
+                    isActive={index===activeIndex}
                   />
                 </div>
               </CarouselItem>
             ))}
           </CarouselContent>
+          </div>
 
-          <CarouselPrevious className="text-white-900 absolute top-1/2 left-10 z-20 size-12 -translate-y-1/2 rounded-full border-2 border-white/0 bg-white/0 opacity-100 shadow-2xl backdrop-blur-md hover:bg-white/50" />
-          <CarouselNext className="text-white-900 absolute top-1/2 right-10 z-20 size-12 -translate-y-1/2 rounded-full border-2 border-white/0 bg-white/0 opacity-100 shadow-2xl backdrop-blur-md hover:bg-white/50" />
+          <CarouselPrevious
+            className="text-white-900 absolute top-1/2 left-10 z-20 size-12 -translate-y-1/2 rounded-full border-2 border-white/0 bg-white/0 opacity-100 shadow-2xl backdrop-blur-md hover:bg-white/50" />
+          <CarouselNext 
+            className="text-white-900 absolute top-1/2 right-10 z-20 size-12 -translate-y-1/2 rounded-full border-2 border-white/0 bg-white/0 opacity-100 shadow-2xl backdrop-blur-md hover:bg-white/50" />
         </Carousel>
       </section>
     </main>
--- src/components/seasons/2026/blocks/judges/profile-card.tsx
+++ src/components/seasons/2026/blocks/judges/profile-card.tsx
@@ -1,7 +1,7 @@
 import { cn } from "@/lib/utils";
 import DOTImage from "@components/common/dot-image";
 import { LinkedinIconStroke } from "@components/icons/linkedin";
-import { Judge } from "@components/seasons/2025/blocks/judges";
+import { Judge } from "@components/seasons/2026/blocks/judges";
 import { Card, CardContent, CardHeader } from "@components/ui/card";
 import Link from "next/link";
 
@@ -9,41 +9,68 @@ type Props = {
   profile: Judge;
   isInView: boolean;
   index: number;
+  isActive: boolean;
 };
 
-function ProfileCard({ profile, isInView, index }: Props) {
+function ProfileCard({ profile, isInView, index, isActive}: Props) {
+    index > 0 ? "translate-x-80" : "";
   return (
     <Card
       className={cn(
-        "relative h-[380px] w-[380px] rounded-full border-0 bg-white/60 p-8 opacity-0 transition-all duration-500 ease-out-quart",
-        { "translate-y-0 opacity-100": isInView },
+        "relative h-[500px] w-[500px] rounded-full border-0 bg-white/60 p-8 opacity-0 transition-all duration-500 ease-out-quart",
+        isInView && "translate-y-0 opacity-100",
+        isActive 
+        ? "scale-100 z-20 shadow-2x1"
+        : "scale-50 opacity-60"
       )}
       style={{
         transitionDelay: `${index * 50}ms`,
       }}
     >
-      <CardHeader className="absolute top-4 left-1/2 h-48 w-48 -translate-x-1/2 overflow-hidden rounded-full bg-[#fefcb]">
+      <CardHeader className= {cn(
+        "absolute overflow-hidden rounded-full bg-[#fefcb]",
+        isActive 
+        ?  "h-56 w-56 left-35"
+        : "inset-0 flex items-center justify-center scale-130"
+      )} 
+      >
+      {isActive ? (
+        <>
         <DOTImage
           alt={profile.name}
           src={profile.imageURL}
           fill
           sizes="(min-width: 0px) 50vw; (min-width: 768px) 25vw"
         />
+        </>
+      ) : (
+        <DOTImage
+          alt={profile.name} 
+          src={`/images/seasons/2026/landing/judges/planets/${(index % 5) + 1}.png`}
+          fill
+          sizes="(min-width: 0px) 50vw; (min-width: 768px) 25vw"
+          className="object-contain"
+        />
+      )}
       </CardHeader>
-      <CardContent className="mt-48 px-4 text-center text-sm leading-tight text-(--blue)">
+
+      <CardContent className="mt-62 px-4 text-center text-sm leading-tight text-(--blue)">
         <div className="space-y-2">
           <h3
             className="text-center text-2xl leading-[1.1] font-bold lg:text-3xl xl:text-4xl"
             // Force newline between first and last name
-            style={{ wordSpacing: "100vw" }}
+            //style={{ wordSpacing: "100vw" }}
           >
             {profile.name}
           </h3>
-          <p className="lg:text-base3xl:text-xl -mx-3 text-sm font-medium 3xl:text-lg">
+          <p className="lg:text-base3xl:text-xl -mx-3 text-xl font-medium 3xl:text-x1">
             {profile.position}
           </p>
+          <p className="lg:text-base3xl:text-xl -mx-3 text-xl font-medium 3xl:text-lg leading-[1.15]">
+            {profile.company}
+          </p>
         </div>
-        <div className="flex items-center justify-center gap-2">
+        <div className="flex items-center justify-center gap-2 mt-2">
           <Link
             href={profile.linkedInURL}
             className="rounded-full bg-(--blue) p-2 text-white"
