953d955072cb2b12fb46d2ff25f4e7535fb9d947
Jay Chan <jayc10@uci.edu>
2026-03-11 04:09:44 -0700

    fix: background gradient full length

--- src/components/seasons/2026/blocks/landing/landing-2026.tsx
+++ src/components/seasons/2026/blocks/landing/landing-2026.tsx
@@ -16,7 +16,7 @@ import MeetCommittees from "@components/seasons/2026/blocks/team";
 
 export default function Landing2026() {
   return (
-    <main className="relative min-h-[100dvh] w-full overflow-visible bg-[url('/images/seasons/2026/landing/gradient/background.webp')] bg-cover bg-top bg-repeat">
+    <main className="relative min-h-[100dvh] w-full overflow-visible bg-[url('/images/seasons/2026/landing/gradient/background.webp')] bg-[length:100%_auto] bg-top bg-repeat">
       <LandingNavigation />
       <Hero />
       <About />
