61e360d32a2df11196d75dc1b21e410e4a32ec12
Allison <allisonlyhuang@gmail.com>
2026-03-13 20:34:12 -0700

    fix: null ctx value

--- src/components/seasons/2026/common/sparkles.tsx
+++ src/components/seasons/2026/common/sparkles.tsx
@@ -8,7 +8,7 @@ export default function Sparkles() {
   useEffect(() => {
     const canvas = canvasRef.current;
     if (!canvas) return;
-    const ctx = canvas.getContext("2d");
+    const ctx = canvas.getContext("2d")!;
     if (!ctx) return;
 
     const dpr = window.devicePixelRatio || 1;
