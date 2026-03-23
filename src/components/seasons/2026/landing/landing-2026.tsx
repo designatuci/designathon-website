8a9b18e5ae958b866737a7b14a2d04e79a013bd6
Jay Chan <jayc10@uci.edu>
2026-03-23 00:54:29 -0700

    chore: styling reorder

--- src/components/seasons/2026/landing/landing-2026.tsx
+++ src/components/seasons/2026/landing/landing-2026.tsx
@@ -6,8 +6,6 @@ import Footer from "@components/seasons/2026/blocks/footer";
 import Hero from "@components/seasons/2026/blocks/hero";
 import Judges from "@components/seasons/2026/blocks/judges";
 import MailingList from "@components/seasons/2026/blocks/mailing";
-import MeetCommittees from "@components/seasons/2026/blocks/team";
-import LandingNavigation from "@components/seasons/2026/navigation/landing-navigation";
 import Partners from "@components/seasons/2026/blocks/partners";
 import PastEvents from "@components/seasons/2026/blocks/past-events";
 import Prizes from "@components/seasons/2026/blocks/prizes";
@@ -15,10 +13,12 @@ import Prompt from "@components/seasons/2026/blocks/prompt";
 import Rules from "@components/seasons/2026/blocks/rules";
 import Schedule from "@components/seasons/2026/blocks/schedule";
 import Star from "@components/seasons/2026/blocks/star";
+import MeetCommittees from "@components/seasons/2026/blocks/team";
+import LandingNavigation from "@components/seasons/2026/navigation/landing-navigation";
 
 export default function Landing2026() {
   return (
-    <main className="relative noise min-h-[100dvh] w-full overflow-hidden bg-[url('/images/seasons/2026/landing/gradient/background.webp')] bg-[length:100%_auto] bg-top bg-repeat-y">
+    <main className="noise relative min-h-[100dvh] w-full overflow-hidden bg-[url('/images/seasons/2026/landing/gradient/background.webp')] bg-[length:100%_auto] bg-top bg-repeat-y">
       <LandingNavigation />
       <Hero />
       <About />
--- src/components/seasons/2026/navigation/landing-navigation.tsx
+++ src/components/seasons/2026/navigation/landing-navigation.tsx
@@ -163,4 +163,3 @@ function LandingNavigationDesktop() {
     </div>
   );
 }
-
